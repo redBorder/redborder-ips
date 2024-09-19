b434b9157057ca04d503240e46c8ecbe75b6d6b6
Luis Blanco <ljblanco@redborder.com>
2024-09-19 13:33:41 +0100

    Revert "lint line too long"
    This reverts commit 98ade997618c75f1e1135f3f18187f0e5f992a30.

--- resources/scripts/rb_change_segments.rb
+++ resources/scripts/rb_change_segments.rb
@@ -116,7 +116,7 @@ cancel_change_segments if segments_conf.cancel
 def create_or_update_network_scripts(segment, init_conf)
   logger = Logger.new(STDOUT)
   logger.info("Starting update for segment: #{segment['name']}")
-  if segment['ports'].empty?
+  if segment["ports"].empty?
     return logger.warn("No ports to configure for segment #{segment['name']}. Skipping configuration.")
   end
 
