b5205fa1e40a87755e8690d63944046d12874ab5
ljblancoredborder <ljblanco@redborder.com>
2026-03-27 15:58:22 +0000

    show errors on http socket connection

--- resources/scripts/rb_init_conf.rb
+++ resources/scripts/rb_init_conf.rb
@@ -346,19 +346,28 @@ if opt["r"]
     system("sudo hostnamectl set-hostname #{ips_node_name}")
     Config_utils.ensure_log_file_exists
     system("echo 'Sensor #{ips_node_name} association in progress...' > #{Config_utils.log_file}")
-    system("/usr/lib/redborder/scripts/rb_associate_sensor.rb -u #{webui_user} -p #{webui_pass} -i #{Config_utils.get_ip_address} -m #{webui_host} >> #{Config_utils.log_file} 2>&1")
-    if $?.exitstatus == 0
+    cmd = "/usr/lib/redborder/scripts/rb_associate_sensor.rb -u #{webui_user} -p #{webui_pass} -i #{Config_utils.get_ip_address} -m #{webui_host}"
+
+    output = `#{cmd} 2>&1`
+    exit_status = $?.exitstatus
+
+    # Always append to log
+    File.open(Config_utils.log_file, "a") { |f| f.puts(output) }
+
+    if exit_status == 0
       Config_utils.hook_hosts(webui_host, cdomain)
       Config_utils.replace_chef_server_url(cdomain)
-      
+
       system("sed -i '/webui_pass/d' #{INITCONF}")
       puts "Sensor registered to the manager, please wait..."
       puts "You can see logs in #{Config_utils.log_file}"
       system("/usr/lib/redborder/bin/rb_register_finish.sh >> #{Config_utils.log_file} 2>&1")
       puts "Registration and configuration finished!"
     else
-      
-      puts "Error: Sensor association failed with exit status #{$?.exitstatus}."
+      puts "Error: Sensor association failed with exit status #{exit_status}."
+      puts "---- Command output ----"
+      puts output
+      puts "------------------------"
       puts "Please review #{INITCONF} file or network configuration..."
       puts "See \"#{Config_utils.log_file}\" for more details."
     end
